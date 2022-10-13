3716d7d8a6b2eac0e2f18ca7f57f9b2ebd325c1a
andresilva8624@gmail.com <andresilva8624@gmail.com>
2022-10-13 11:15:06 -0700

    solved issues

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
-DROP DATABASE IF EXISTS employess;
+DROP DATABASE IF EXISTS employees;
 
 CREATE DATABASE employees;
 
@@ -21,7 +21,7 @@ CREATE TABLE role (
     CREATE TABLE employee (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) UNIQUE NOT NULL,
-    last_name DECIMAL UNIQUE NOT NULL, 
+    last_name VARCHAR UNIQUE NOT NULL, 
     role_id INT UNSIGNED NOT NULL,
     INDEX role_ind (role_id),
     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
